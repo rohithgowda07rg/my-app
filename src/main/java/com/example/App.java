d08a0bd04676e1395c65e24be854f12d94ab31be
rohithgowda07rg <rohithgowdadu07@gmail.com>
2026-03-24 22:38:01 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,1 +1,11 @@
+package com.example;
 
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins CI/CD Pipeline!");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
